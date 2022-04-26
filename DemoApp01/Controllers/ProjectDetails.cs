5018c7d8e9278cb35fdb980ea967fa3de1707ca4
tanveerm <tanveerm@TANVEERM-CORP-L>
2022-04-26 16:01:04 +0530

    Add project files.

---
+++ DemoApp01/Controllers/ProjectDetails.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Linq;
+
+namespace DemoApp01.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+
+    public class ProjectDetails : ControllerBase
+    {
+        private static readonly string[] ProjectName = new[]
+        {
+            "ProjectOne", "ProjectTwo", "ProjectThree", "ProjectFour","ProjectFive"
+        };
+
+        private readonly ILogger<ProblemDetails>? logger;
+
+        public int Name { get; private set; }
+        public string? Discription { get; private set; }
+        public DateTime Date { get; private set; }
+
+        [HttpGet(Name = "ProjectDetails")]
+        public IEnumerable<ProjectDetails> Get()
+        {
+            return Enumerable.Range(1, 4).Select(index => new ProjectDetails
+              {
+                Name = Random.Shared.Next(ProjectName.Length),
+                Discription = "Discribing this side of the world!",
+                Date = DateTime.Now.AddDays(index),    
+                })
+                .ToArray();
+        }
+       
+    }
+}
---
+++ DemoApp01/ProjectDetails.cs
@@ -0,0 +1,11 @@
+﻿namespace DemoApp01
+{
+    public class ProjectDetails
+    {
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+
+        public DateTime Date { get; set; }
+
+    }
+}
